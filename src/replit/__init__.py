9d9f6a0bc8757a5672d96fb3e25609599e9cdd76
Devon Stewart <devon.stewart@repl.it>
2024-07-18 13:10:19 -0700

    Resolving #13

--- src/replit/__init__.py
+++ src/replit/__init__.py
@@ -1,1 +1,36 @@
-"""Root interface for replit packages."""
+# flake8: noqa
+
+"""The Replit Python module."""
+
+try:
+    # This is inlined from https://github.com/replit/replit-py/blob/master/src/replit/__init__.py
+    # See https://github.com/replit/replit-object-storage-python/issues/13 for context
+    from typing import Any
+    from . import database, web
+    from .database import (
+        Database,
+        AsyncDatabase,
+        make_database_proxy_blueprint,
+        start_database_proxy,
+    )
+    from .info import ReplInfo
+
+    info = ReplInfo()
+
+
+    # Backwards compatibility.
+    def clear() -> None:
+        """Clear the terminal."""
+        print("\033[H\033[2J", end="", flush=True)
+
+
+    # Previous versions of this library would just have side-effects and always set
+    # up a database unconditionally. That is very undesirable, so instead of doing
+    # that, we are using this egregious hack to get the database / database URL
+    # lazily.
+    def __getattr__(name: str) -> Any:
+        if name == "db":
+            return database.db
+        raise AttributeError(name)
+except ImportError:
+  pass
